28684e6b94f93f69b25a38f8caeb639250061e94
flatychen <flatychen@126.com>
2019-01-30 09:51:57 +0800

    init

---
+++ src/main/java/cn/flaty/concurrent/learn/ConcurrentLearnApplication.java
@@ -0,0 +1,14 @@
+package cn.flaty.concurrent.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentLearnApplication.class, args);
+    }
+
+}
+

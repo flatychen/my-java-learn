e89c5f8d8d51b966a150bf58885c4719677fde18
flatychen <flatychen@126.com>
2019-04-24 09:46:32 +0800

    xxx

--- src/main/java/cn/flaty/leetcode/medium/Subsets.java
+++ src/main/java/cn/flaty/leetcode/medium/Subsets.java
@@ -13,34 +13,18 @@ public class Subsets {
         List<List<Integer>> alls = new ArrayList<>(nums.length);
         Arrays.sort(nums);
         this.Subsets(alls, new ArrayList<>(), 0, nums);
-//        this.backtrack(alls, new ArrayList<>(), nums, 0);
         return alls;
-    }
-
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
-        list.add(new ArrayList<>(tempList));
-        System.out.println(Arrays.toString(tempList.toArray()));
-        for (int i = start; i < nums.length; i++) {
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, i + 1);
-            tempList.remove(tempList.size() - 1);
-        }
     }
 
 
     public void Subsets(List<List<Integer>> all, List<Integer> tempList, int deep, int[] nums) {
         // 最后一行退出
-//        all.add(new ArrayList<>(tempList));
-
+        all.add(new ArrayList<>(tempList));
         if (deep <= nums.length) {
             System.out.println(Arrays.toString(tempList.toArray()));
         }
 
         for (int i = deep; i < nums.length; i++) {
-//            if (i > 0 && nums[i] == nums[i - 1]) {
-//                continue;
-//            }
-
             tempList.add(nums[i]);
             Subsets(all, tempList, i + 1, nums);
             tempList.remove(tempList.size() - 1);
